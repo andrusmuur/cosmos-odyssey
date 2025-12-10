d900f36b612a4c845fca48349ec640b8464b964e
Andrus Müür <andrus.muur@gmail.com>
2025-12-10 09:54:18 +0200

    first commit

---
+++ src/test/java/com/example/cosmos_odyssey/CosmosOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosmos_odyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosOdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
